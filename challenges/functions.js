238fadde3ea1b62b14daae76bf3942603bdd97fc
evansibok <evansibok@gmail.com>
2019-08-23 09:32:25 +0100

    ceated consume function

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,12 +7,17 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(param1, param2, cb){
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
