2ae5bb00504c9618357ddb49599d7d07148f4c1d
evansibok <evansibok@gmail.com>
2019-08-23 17:21:34 +0100

    completed request 1 and 2 in ADVANCED ARRAY METHODS on zooAnimals

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -15,30 +15,30 @@
 // Using your dinosaur objects, log answers to these questions:
 
 let tyrannosaurus = {
-  name: "tyrannosaurus",
-  diet: "carnivorous",
-  weight: "7000kg",
-  length: "12m",
-  period: "Late Cretaceous",
-  roar: function() {
-    return `RAWERSRARARWERSARARARRRR!`;
-  }
+	name: "tyrannosaurus",
+	diet: "carnivorous",
+	weight: "7000kg",
+	length: "12m",
+	period: "Late Cretaceous",
+	roar: function () {
+		return `RAWERSRARARWERSARARARRRR!`;
+	}
 };
 
 let stegosaurus = {
-  name: "stegosaurus",
-  diet: "herbivorous",
-  weight: "2000kg",
-  length: "9m",
-  period: "Late Jurassic"
+	name: "stegosaurus",
+	diet: "herbivorous",
+	weight: "2000kg",
+	length: "9m",
+	period: "Late Jurassic"
 };
 
 let velociraptor = {
-  name: "velociraptor",
-  diet: "carnivorous",
-  weight: "15kg",
-  length: "1.8m",
-  period: "Late Cretaceous"
+	name: "velociraptor",
+	diet: "carnivorous",
+	weight: "15kg",
+	length: "1.8m",
+	period: "Late Cretaceous"
 };
 
 // How much did tyrannosaurus weigh?
@@ -61,67 +61,67 @@ console.log(tyrannosaurus.roar());
 // Given an array of college graduates.  Complete the following requests using any array method you like
 
 const graduates = [
-  {
-    id: 1,
-    first_name: "Cynde",
-    university: "Missouri Southern State College",
-    email: "ctorry0@macromedia.com"
-  },
-  {
-    id: 2,
-    first_name: "Saundra",
-    university: "The School of the Art Institute of Chicago",
-    email: "swhal1@state.gov"
-  },
-  {
-    id: 3,
-    first_name: "Lambert",
-    university: "Marian College",
-    email: "lparham2@techcrunch.com"
-  },
-  {
-    id: 4,
-    first_name: "Modestine",
-    university: "International Medical & Technological University",
-    email: "mdolder3@symantec.com"
-  },
-  {
-    id: 5,
-    first_name: "Chick",
-    university: "Sultan Salahuddin Abdul Aziz Shah Polytechnic",
-    email: "camorts4@google.com.au"
-  },
-  {
-    id: 6,
-    first_name: "Jakob",
-    university:
-      "Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft",
-    email: "jharken5@spiegel.de"
-  },
-  {
-    id: 7,
-    first_name: "Robbi",
-    university: "Salem University",
-    email: "rbrister6@redcross.org"
-  },
-  {
-    id: 8,
-    first_name: "Colline",
-    university: "Coastal Carolina University",
-    email: "cbrosh7@alibaba.com"
-  },
-  {
-    id: 9,
-    first_name: "Michail",
-    university: "Universidad Católica de Ávila",
-    email: "mrome8@shinystat.com"
-  },
-  {
-    id: 10,
-    first_name: "Hube",
-    university: "Universitat Rovira I Virgili Tarragona",
-    email: "hlethbrig9@foxnews.com"
-  }
+	{
+		id: 1,
+		first_name: "Cynde",
+		university: "Missouri Southern State College",
+		email: "ctorry0@macromedia.com"
+	},
+	{
+		id: 2,
+		first_name: "Saundra",
+		university: "The School of the Art Institute of Chicago",
+		email: "swhal1@state.gov"
+	},
+	{
+		id: 3,
+		first_name: "Lambert",
+		university: "Marian College",
+		email: "lparham2@techcrunch.com"
+	},
+	{
+		id: 4,
+		first_name: "Modestine",
+		university: "International Medical & Technological University",
+		email: "mdolder3@symantec.com"
+	},
+	{
+		id: 5,
+		first_name: "Chick",
+		university: "Sultan Salahuddin Abdul Aziz Shah Polytechnic",
+		email: "camorts4@google.com.au"
+	},
+	{
+		id: 6,
+		first_name: "Jakob",
+		university:
+			"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft",
+		email: "jharken5@spiegel.de"
+	},
+	{
+		id: 7,
+		first_name: "Robbi",
+		university: "Salem University",
+		email: "rbrister6@redcross.org"
+	},
+	{
+		id: 8,
+		first_name: "Colline",
+		university: "Coastal Carolina University",
+		email: "cbrosh7@alibaba.com"
+	},
+	{
+		id: 9,
+		first_name: "Michail",
+		university: "Universidad Católica de Ávila",
+		email: "mrome8@shinystat.com"
+	},
+	{
+		id: 10,
+		first_name: "Hube",
+		university: "Universitat Rovira I Virgili Tarragona",
+		email: "hlethbrig9@foxnews.com"
+	}
 ];
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
@@ -139,7 +139,7 @@ Name email@example.com
 Log the result of your new array. */
 const contactInfo = [];
 
-graduates.forEach(function(value) {
+graduates.forEach(function (value) {
 	contactInfo.push(`${value.first_name} ${value.email}`);
 	return contactInfo;
 });
@@ -148,8 +148,8 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
 
-graduates.filter(function(value) {
-	if (value.university.includes("Uni")){
+graduates.filter(function (value) {
+	if (value.university.includes("Uni")) {
 		uni.push(value.university);
 		return uni;
 	}
@@ -161,66 +161,66 @@ console.log(uni);
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
 
 const zooAnimals = [
-  {
-    animal_name: "Jackal, asiatic",
-    population: 5,
-    scientific_name: "Canis aureus",
-    state: "Kentucky"
-  },
-  {
-    animal_name: "Screamer, southern",
-    population: 1,
-    scientific_name: "Chauna torquata",
-    state: "Alabama"
-  },
-  {
-    animal_name: "White spoonbill",
-    population: 8,
-    scientific_name: "Platalea leucordia",
-    state: "Georgia"
-  },
-  {
-    animal_name: "White-cheeked pintail",
-    population: 1,
-    scientific_name: "Anas bahamensis",
-    state: "Oregon"
-  },
-  {
-    animal_name: "Black-backed jackal",
-    population: 2,
-    scientific_name: "Canis mesomelas",
-    state: "Washington"
-  },
-  {
-    animal_name: "Brolga crane",
-    population: 9,
-    scientific_name: "Grus rubicundus",
-    state: "New Mexico"
-  },
-  {
-    animal_name: "Common melba finch",
-    population: 5,
-    scientific_name: "Pytilia melba",
-    state: "Pennsylvania"
-  },
-  {
-    animal_name: "Pampa gray fox",
-    population: 10,
-    scientific_name: "Pseudalopex gymnocercus",
-    state: "Connecticut"
-  },
-  {
-    animal_name: "Hawk-eagle, crowned",
-    population: 10,
-    scientific_name: "Spizaetus coronatus",
-    state: "Florida"
-  },
-  {
-    animal_name: "Australian pelican",
-    population: 5,
-    scientific_name: "Pelecanus conspicillatus",
-    state: "West Virginia"
-  }
+	{
+		animal_name: "Jackal, asiatic",
+		population: 5,
+		scientific_name: "Canis aureus",
+		state: "Kentucky"
+	},
+	{
+		animal_name: "Screamer, southern",
+		population: 1,
+		scientific_name: "Chauna torquata",
+		state: "Alabama"
+	},
+	{
+		animal_name: "White spoonbill",
+		population: 8,
+		scientific_name: "Platalea leucordia",
+		state: "Georgia"
+	},
+	{
+		animal_name: "White-cheeked pintail",
+		population: 1,
+		scientific_name: "Anas bahamensis",
+		state: "Oregon"
+	},
+	{
+		animal_name: "Black-backed jackal",
+		population: 2,
+		scientific_name: "Canis mesomelas",
+		state: "Washington"
+	},
+	{
+		animal_name: "Brolga crane",
+		population: 9,
+		scientific_name: "Grus rubicundus",
+		state: "New Mexico"
+	},
+	{
+		animal_name: "Common melba finch",
+		population: 5,
+		scientific_name: "Pytilia melba",
+		state: "Pennsylvania"
+	},
+	{
+		animal_name: "Pampa gray fox",
+		population: 10,
+		scientific_name: "Pseudalopex gymnocercus",
+		state: "Connecticut"
+	},
+	{
+		animal_name: "Hawk-eagle, crowned",
+		population: 10,
+		scientific_name: "Spizaetus coronatus",
+		state: "Florida"
+	},
+	{
+		animal_name: "Australian pelican",
+		population: 5,
+		scientific_name: "Pelecanus conspicillatus",
+		state: "West Virginia"
+	}
 ];
 
 /* Request 1: .forEach()
@@ -229,6 +229,12 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+
+zooAnimals.forEach(function (names) {
+	animalNames.push(
+		{ animal_name: names.animal_name, scientific_name: names.scientific_name });
+	return animalNames;
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -238,6 +244,11 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 */
 
 const lowerCase = [];
+
+zooAnimals.map(function(names) {
+	lowerCase.push(names.animal_name.toLowerCase());
+	return lowerCase;
+})
 console.log(lowerCase);
 
 /* Request 3: .filter() 
